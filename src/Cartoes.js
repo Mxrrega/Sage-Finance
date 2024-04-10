3339fd0b540a5147426169d31b716446d8213f1d
Vitorbelfi <vitorabm2015@gmail.com>
2024-04-10 09:16:40 -0300

    Alteração style transações e cartões

--- src/Cartoes.js
+++ src/Cartoes.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { Text, View, TouchableOpacity, Image } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
@@ -36,7 +36,7 @@ export default function Cartoes({handle}) {
       </View>
 
       {renderDivision(require("./images/Mastercard-logo.svg.png"), "Cartão Mastercard")}
-      {renderDivision(require("./images/cartaoelo.png"), "Outro texto na frente da imagem")}
+      {renderDivision(require("./images/cartaoelo.png"), "Cartão Elo")}
     </View>
   );
 }
@@ -97,5 +97,133 @@ const styles = {
     marginLeft: 10, 
     color: "white",
   },
-};
+};*/
 
+
+
+import React from "react";
+import { Text, View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+
+import MastercardLogo from "./images/Mastercard-logo.svg.png";
+import CartaoElo from "./images/cartaoelo.png";
+
+export default function Cartoes({ handle }) {
+
+  const mastercardImage = MastercardLogo;
+  const eloImage = CartaoElo;
+
+  const renderDivision = (source, text, imageStyle) => (
+    <View style={styles.division}>
+    
+      <Image source={source} style={[styles.image, imageStyle]} />
+      <Text style={styles.text}>{text}</Text>
+      <Text style={styles.faturaAberta}>Fatura Aberta</Text>
+    </View>
+  );
+
+  return (
+    <View>
+      <StatusBar />
+
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => handle(false)}>
+          <Text style={styles.backIcon}>&#8592;</Text>
+        </TouchableOpacity>
+        <View style={styles.headerTextContainer}>
+          <Text style={styles.headerText}>Cartão de Crédito</Text>
+          <View style={styles.limiteContainer}>
+            <FontAwesome name="money" size={24} color="white" />
+            <View style={styles.limiteTextContainer}>
+              <Text style={styles.limite}>Limite Disponível</Text>
+              <Text style={styles.valor}>R$ 1000,00</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+    
+      {renderDivision(mastercardImage, mastercardText, styles.mastercardImage)}
+      {renderDivision(eloImage, eloText, styles.eloImage)}
+     
+    </View>
+  );
+}
+
+// Constantes para os estilos de texto
+const mastercardText = "Cartão Mastercard";
+const eloText = "Cartão Elo";
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#2C3E50",
+    padding: 30,
+    flexDirection: "row",
+  },
+  backIcon: {
+    color: "white",
+    fontSize: 50,
+    marginTop: -29,
+  },
+  headerTextContainer: {
+    marginTop: 5,
+  },
+  headerText: {
+    fontWeight: "bold",
+    color: "white",
+    marginTop: -5,
+    fontSize: 24,
+  },
+  limiteContainer: {
+    flexDirection: "row",
+    marginTop: 50,
+    right: 60,
+  },
+  limiteTextContainer: {
+    marginLeft: 12,
+  },
+  limite: {
+    top: -10,
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  valor: {
+    color: "white",
+    top: -10,
+  },
+  division: {
+    backgroundColor: "#2C3E50",
+    height: 120,
+    marginVertical: 18,
+    marginHorizontal: 9,
+    borderRadius: 20,
+    paddingLeft: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    width: 70,
+    height: 50,
+  },
+  text: {
+    marginLeft: 10,
+    color: "white",
+  },
+  faturaAberta: {
+    color: "white",
+    position: "absolute",
+    bottom: 10,
+    left: 10,
+  },
+ 
+  mastercardImage: {
+    width: 50,
+    height: 40,
+  },
+  eloImage: {
+    width: 70,
+    height: 20,
+  },
+});
--- src/Transacoes.js
+++ src/Transacoes.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { FontAwesome } from '@expo/vector-icons'; // Importar FontAwesome
 
@@ -106,12 +106,210 @@ const styles = StyleSheet.create({
     marginTop: 20, 
     marginBottom: 10, 
   },
+});*/
+
+
+
+
+
+
+/*import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'; // Importar FontAwesome
+
+const TransacaoItem = ({ icon, icon2, color, text1, text2 }) => {
+  return (
+    <View style={styles.bottomContainer}>
+      <FontAwesome name={icon} style={styles.icon} color={color} />
+      <View style={styles.subBottomContainer}>
+        <Text style={styles.subBottomText2}>{text1}</Text>
+        <FontAwesome name={icon2} style={styles.icon2} color={color} />
+        <Text style={[styles.bottomText, { marginLeft: 10 }]}>{text2}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default function Transacoes() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Transações</Text>
+      </View>
+      <TransacaoItem  icon2="arrow-down" color="green" text1="Balanço Mensal" text2="R$1000" />
+      <TransacaoItem  icon2="arrow-down" color="green" text1="Saldo Atual" text2="R$1000" />
+      <Text style={styles.diasemana}>Quarta-Feira - dia 5</Text>
+      <TransacaoItem icon="shopping-cart" icon2="arrow-down" color="black" text1="Mercado" text2="R$500" />
+      <TransacaoItem icon="money" icon2="arrow-up" color="black" text1="Investimentos" text2="R$750" />
+      <TransacaoItem icon="user-circle" icon2="arrow-down" color="black" text1="Contas a pagar" text2="R$1200" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#2980B9',
+    padding: 50,
+    alignItems: 'flex-start',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  bottomText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  subBottomContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  subBottomText: {
+    fontSize: 14,
+    marginLeft: 5,
+  },
+  subBottomText2: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  icon: {
+    fontSize: 50,
+    marginRight: 5,
+  },
+  icon2: {
+    fontSize: 30,
+    marginRight: 5,
+  },
+  diasemana: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+});*/
+
+
+
+
+
+
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'; // Importar FontAwesome
+
+// Componente reutilizável para representar cada item de transação
+const TransacaoItem = ({ icon, icon2, color, text1, text2 }) => {
+  return (
+    <View style={styles.bottomContainer}>
+      <FontAwesome name={icon} style={styles.icon} color={color} />
+      <View style={styles.subBottomContainer}>
+        <Text style={styles.subBottomText2}>{text1}</Text>
+        <View style={styles.subBottomTextContainer}>
+          <FontAwesome name={icon2} style={styles.icon2} color={color} />
+          <Text style={[styles.bottomText, { marginLeft: 10 }]}>{text2}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default function Transacoes() {
+ 
+  const balancoMensal = <TransacaoItem icon2="dollar" color="red" text1="Balanço Mensal" text2="R$1000" />;
+  const saldoAtual = <TransacaoItem icon2="dollar" color="green" text1="Saldo Atual" text2="R$1000" />;
+  const mercado = <TransacaoItem icon="shopping-cart" icon2="arrow-down" color="black" text1="Mercado" text2="R$500" />;
+  const investimentos = <TransacaoItem icon="money" icon2="arrow-up" color="black" text1="Investimentos" text2="R$750" />;
+  const contasPagar = <TransacaoItem icon="user-circle" icon2="arrow-down" color="black" text1="Contas a pagar" text2="R$1200" />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Transações</Text>
+      </View>
+      {balancoMensal}
+      {saldoAtual}
+      <Text style={styles.diasemana}>Quarta-Feira - dia 5</Text>
+      {mercado}
+      {investimentos}
+      {contasPagar}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#2C3E50',
+    padding: 50,
+    alignItems: 'flex-start',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  bottomText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  subBottomContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between', // Ajuste para distribuir os elementos ao longo do eixo principal
+    flex: 1, // Permitir que o subBottomContainer ocupe todo o espaço disponível
+  },
+  subBottomText: {
+    fontSize: 14,
+    marginLeft: 5,
+  },
+  subBottomText2: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  icon: {
+    fontSize: 50,
+    marginRight: 5,
+  },
+  icon2: {
+    fontSize: 30,
+    marginRight: 5,
+  },
+  diasemana: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
 });
-
-
-
-
-
-
-
-
